9ade2b35518bd8a2eb251dea37e3804eda32921b
Alexey Kolosov <alexey.kolosov@gmail.com>
2012-01-16 09:22:00 +0600

    Небольшие изменения в spec'ах

--- spec/typographica_spec.rb
+++ spec/typographica_spec.rb
@@ -4,12 +4,18 @@ require File.dirname(__FILE__) + '/spec_helper'
 
 describe Typographica do
   describe "try to create typographica.html" do
+
     it "create typographica.html" do
       str = File.open(File.dirname(__FILE__) + '/text2test.html').read().to_s
 
-      File.open("typographica.html", "w").puts str.typography!
+      File.open(File.dirname(__FILE__) + "/typographica.html", "w").puts str.typography!
 
-      File.exists?("typographica.html").should == true
+      File.exists?(File.dirname(__FILE__) + "/typographica.html").should == true
     end
+
+    it "check typographica.html" do
+      File.zero?("typographica.html").should == false
+    end
+
   end
 end
