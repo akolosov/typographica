e61d6f8ce8e55994be12c139d4df825b72b0e982
Alexey Kolosov <alexey.kolosov@gmail.com>
2012-01-16 10:42:00 +0600

    Теперь расстановка "мягких" переносов по умолчанию отключена.

--- spec/typographica_spec.rb
+++ spec/typographica_spec.rb
@@ -3,19 +3,38 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe Typographica do
-  describe "try to create typographica.html" do
+  str = File.open(File.dirname(__FILE__) + '/text2test.html').read().to_s
 
-    it "create typographica.html" do
-      str = File.open(File.dirname(__FILE__) + '/text2test.html').read().to_s
+  describe "try to create html without soft hyphen" do
 
-      File.open(File.dirname(__FILE__) + "/typographica.html", "w").puts str.typography!
+    it "create html without soft hyphen" do
 
-      File.exists?(File.dirname(__FILE__) + "/typographica.html").should == true
+      File.open(File.dirname(__FILE__) + "/typographica-no-shy.html", "w").puts str.typography!
+
+      File.exists?(File.dirname(__FILE__) + "/typographica-no-shy.html").should == true
     end
 
-    it "check typographica.html" do
-      File.zero?("typographica.html").should == false
+    it "check typographica-no-shy.html" do
+      File.zero?("typographica-no-shy.html").should == false
     end
 
   end
+
+  describe "try to create html with soft hyphen" do
+
+    it "create html with soft hyphen" do
+
+      File.open(File.dirname(__FILE__) + "/typographica-shy.html", "w").puts str.typography!(true)
+
+      File.exists?(File.dirname(__FILE__) + "/typographica-shy.html").should == true
+    end
+
+    it "check typographica-shy.html" do
+      File.zero?("typographica-shy.html").should == false
+    end
+
+  end
+
+puts 'Русский текст,без "обработки" и корректировки-шмаректировки.'.typography!
+puts 'Русский текст,без "обработки" и корректировки-шмаректировки.'.typography!(true)
 end
